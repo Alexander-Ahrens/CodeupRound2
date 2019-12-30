1e71e4bbc2b318bf9be21842bfe7a8d624a6dcfb
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-12-30 11:34:01 -0600

    Created and finished operators.js exercise.

---
+++ js/operators.js
@@ -0,0 +1,33 @@
+'1' + 2; // = 12
+
+typeof '1' + 2; // = string2
+
+6 % 4; // = 2
+
+'3 + 4 is ' + 3 + 4; // = '3 + 4 is 34
+//make this produce the correct output
+
+0 < 0; // = false
+
+'false' == false; // = false
+
+true == 'true'; // = false
+
+5 >= -5; // = true
+
+!false || false; // = true
+
+true || "42"; // = true
+
+5 < 4 && 1 === 1; // = false
+
+typeof 'codeup' === 'string'; // = true
+
+'codeup' === 'codeup' && 'codeup' === 'Codeup'; // = false
+
+4 >= 0 && 1 !== '1'; // = true
+
+6 % 3 === 0; // = true
+
+5 % 2 !== 0; // = true
+
